447eb95c35a22c02da3562a16e4fa7e2273fab20
Bagdad Youcef <71285835+YoussBGD@users.noreply.github.com>
2024-05-11 00:05:37 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ import os
 
 
 if __name__ == "__main__":
-    fastapi_process = subprocess.Popen(["uvicorn", "app:app", "--reload", "--host", "0.0.0.0", "--port", "8001"])
-    streamlit_process = subprocess.Popen(["streamlit", "run", "--server.address=0.0.0.0", "streamlit_app.py"])
+    fastapi_process = subprocess.Popen(["uvicorn", "app:app", "--reload", "--port", "8001"])
+    streamlit_process = subprocess.Popen(["streamlit", "run", "streamlit_app.py"])
 
     try:
         fastapi_process.wait()
